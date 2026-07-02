f05aae12b3026e90f6c5a799230fc5ec24f4d989
Afdal Ramdan Daman Huri <afdalramdan@gmail.com>
2026-07-03 05:11:40 +0700

    feat: Add Vercel Analytics and enrich SEO metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import {
   Montserrat,
   Source_Serif_4,
 } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 const montserrat = Montserrat({
@@ -37,8 +38,45 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
-  title: "Afdal & Putri Wedding",
-  description: "Undangan pernikahan Afdal dan Putri.",
+  metadataBase: new URL("https://wedding.afdalrdh.com"),
+  title: "Wedding Afdal & Putri",
+  description:
+    "Undangan pernikahan Afdal dan Putri. Merupakan suatu kehormatan dan kebahagiaan bagi kami apabila Bapak/Ibu/Saudara/i berkenan hadir untuk memberikan doa restu.",
+  keywords: [
+    "Wedding Afdal & Putri",
+    "Wedding Afdal",
+    "Afdal Putri Wedding",
+    "Pernikahan Afdal dan Putri",
+    "Undangan Pernikahan Afdal dan Putri",
+    "Afdal dan Putri",
+  ],
+  authors: [{ name: "Afdal & Putri" }],
+  openGraph: {
+    title: "Wedding Afdal & Putri",
+    description:
+      "Undangan pernikahan Afdal dan Putri. Merupakan suatu kehormatan dan kebahagiaan bagi kami apabila Bapak/Ibu/Saudara/i berkenan hadir untuk memberikan doa restu.",
+    type: "website",
+    locale: "id_ID",
+    url: "https://wedding.afdalrdh.com/",
+    siteName: "Wedding Afdal & Putri",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Wedding Afdal & Putri",
+    description:
+      "Undangan pernikahan Afdal dan Putri. Merupakan suatu kehormatan dan kebahagiaan bagi kami apabila Bapak/Ibu/Saudara/i berkenan hadir untuk memberikan doa restu.",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
 };
 
 export default function RootLayout({
@@ -51,7 +89,10 @@ export default function RootLayout({
       lang="id"
       className={`${montserrat.variable} ${bebasNeue.variable} ${sourceSerif.variable} ${inter.variable} h-full`}
     >
-      <body className="min-h-full">{children}</body>
+      <body className="min-h-full">
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
